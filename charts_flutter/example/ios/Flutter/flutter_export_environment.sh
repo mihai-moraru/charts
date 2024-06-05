d4373f72832721cf1e994ea57e1b7609dc6e34b3
Mihai Moraru <mihai.moraru@maersk.com>
2024-06-05 10:18:12 +0200

    package miss-match fix wip(1)

--- charts_flutter/example/ios/Flutter/flutter_export_environment.sh
+++ charts_flutter/example/ios/Flutter/flutter_export_environment.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/anujpanwar/Anuj2/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/anujpanwar/Anuj2/Workspace/Maersk/charts/charts_flutter/example"
+export "FLUTTER_ROOT=/Users/mihai.moraru/Work/flutter/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/mihai.moraru/Work/flutter/charts/charts_flutter/example"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=/Users/anujpanwar/Anuj2/Workspace/Maersk/charts/charts_flutter/example/lib/main.dart"
+export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
-export "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
-export "PACKAGE_CONFIG=/Users/anujpanwar/Anuj2/Workspace/Maersk/charts/charts_flutter/example/.dart_tool/package_config.json"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
